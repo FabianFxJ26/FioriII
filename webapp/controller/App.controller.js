84ab5eb55b5a1bdc8ee0bfd9d3e8d37c9f8f3f99
Elkin Fabian Alfonso Bermudez <elkin3001@gmail.com>
2024-10-03 17:25:07 +0000

    2.2.UI Development with SAPUI5 Unit 5 Implementing the UI--> Structurâring the UI With Controls-->2.	Using Densities for Controls

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox"
-  ],
-    function (Controller) {
-      "use strict";
-  
-      return Controller.extend("sap.training.exc.controller.App", {
-  
-      });
+  "sap/ui/core/mvc/Controller",
+],
+  function (Controller) {
+    "use strict";
+
+    return Controller.extend("sap.training.exc.controller.App", {
+      onInit: function () {
+        this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+      }
     });
+  });
