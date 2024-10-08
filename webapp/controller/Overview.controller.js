c605a9219d3842c51ef7c510fb9688f85e6e32c1
Elkin Fabian Alfonso Bermudez <elkin3001@gmail.com>
2024-10-08 20:20:57 +0000

    2.2.UI Development with SAPUI5 Unit 5 Implementing the UI--> Unit 7 Models and Data Binding-->6.Sorting and Filtering

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,20 +1,25 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/syncStyleClass",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, syncStyleClass, JSONModel) {
+    function (Controller,
+        syncStyleClass,
+        JSONModel,
+        Filter,
+        FilterOperator) {
         "use strict";
 
         return Controller.extend("sap.training.exc.controller.Overview", {
             onInit: function () {
                 var oModel = new JSONModel();
-                
                 this.getView().setModel(oModel, "customer");
-              },
+            },
             onSave: function () {
                 if (!this.pDialog) {
                     this.pDialog = this.loadFragment({
@@ -35,6 +40,16 @@ sap.ui.define([
             onCustomerChange: function (oEvent) {
                 var oBindingContext = oEvent.getParameter("listItem").getBindingContext();
                 this.byId("bookingTable").setBindingContext(oBindingContext);
+            },
+            onFilterCustomers: function (oEvent) {
+                var aFilter = [];
+                var sQuery = oEvent.getParameter("query");
+                if (sQuery && sQuery.length>0){
+                    aFilter.push(new Filter("Customer Name",FilterOperator.Contains,sQuery));
+                }
+                var oTable = this.byId("customerTable");
+                var oBinding = oTable.getBinding("items");
+                oBinding.Filter(aFilter);
             }
         });
     });
