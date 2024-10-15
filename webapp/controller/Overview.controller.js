cfd37876e3aa555c30fda86b205b5abd781c61b7
Elkin Fabian Alfonso Bermudez <elkin3001@gmail.com>
2024-10-15 16:43:37 +0000

    2.2.UI Development with SAPUI5 Unit 5 Implementing the UI--> Unit 9 oData Model-->2.Creating New Entities through an OData Model

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -3,7 +3,8 @@ sap.ui.define([
     "sap/ui/core/syncStyleClass",
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
+    "sap/ui/model/FilterOperator",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -12,7 +13,8 @@ sap.ui.define([
         syncStyleClass,
         JSONModel,
         Filter,
-        FilterOperator) {
+        FilterOperator,
+        MessageToast) {
         "use strict";
 
         return Controller.extend("sap.training.exc.controller.Overview", {
@@ -21,16 +23,22 @@ sap.ui.define([
                 this.getView().setModel(oModel, "customer");
             },
             onSave: function () {
-                if (!this.pDialog) {
-                    this.pDialog = this.loadFragment({
-                        name: "sap.training.exc.view.Dialog"
-                    }).then(function (oDialog) {
-                        syncStyleClass(this.getOwnerComponent().getContentDensityClass(), this.getView(), oDialog);
-                        return oDialog;
-                    }.bind(this));
-                }
-                this.pDialog.then(function (oDialog) {
-                    oDialog.open();
+                var oModelData = this.getView().getModel("customer").getData();
+                var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+                if (oModelData.Discount === undefined) { oModelData.Discount = 0; }
+                this.byId("customerTable").getBinding("items").create({
+                    "Form": oModelData.Form,
+                    "CustomerName": oModelData.CustomerName,
+                    "Discount": oModelData.Discount,
+                    "Street": oModelData.Street,
+                    "PostCode": oModelData.PostCode,
+                    "City": oModelData.City,
+                    "Country": oModelData.Country,
+                    "Email": oModelData.Email,
+                    "Telephone": oModelData.Telephone
+                }).created().then(function () {
+                    MessageToast.show(oResourceBundle.getText("customerCreatedMessage"));
                 });
             },
 
@@ -44,8 +52,8 @@ sap.ui.define([
             onFilterCustomers: function (oEvent) {
                 var aFilter = [];
                 var sQuery = oEvent.getParameter("query");
-                if (sQuery && sQuery.length>0){
-                    aFilter.push(new Filter("Customer Name",FilterOperator.Contains,sQuery));
+                if (sQuery && sQuery.length > 0) {
+                    aFilter.push(new Filter("Customer Name", FilterOperator.Contains, sQuery));
                 }
                 var oTable = this.byId("customerTable");
                 var oBinding = oTable.getBinding("items");
